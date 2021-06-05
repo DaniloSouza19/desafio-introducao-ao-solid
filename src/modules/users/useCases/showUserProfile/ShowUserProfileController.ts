8bf1f40bbca5c066e066509cee7f358cb6ce7c1a
Danilo Souza <danilo_souza19@outlook.com>
2021-06-05 06:57:57 -0300

    implement ShowUserProfileController

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,18 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const user = this.showUserProfileUseCase.execute({ user_id });
+      return response.json(user);
+    } catch (error) {
+      if (error instanceof Error) {
+        return response.status(404).json({ error: error.message });
+      }
+      console.log(error);
+      return response.status(500).json({ error: "Internal Server Error" });
+    }
   }
 }
 
