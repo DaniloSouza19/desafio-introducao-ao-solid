74a5327c2f9e287fe7038750adf0c847a098f811
Danilo Souza <danilo_souza19@outlook.com>
2021-06-05 06:58:39 -0300

    fix status return and request to params

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,17 +6,17 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    const { user_id } = request.params;
 
     try {
       const user = this.turnUserAdminUseCase.execute({
-        user_id: user_id?.toString(),
+        user_id,
       });
 
       return response.json(user);
     } catch (error) {
       if (error instanceof Error) {
-        return response.status(400).json({ error: error.message });
+        return response.status(404).json({ error: error.message });
       }
       console.log(error);
       return response.status(500).json({ error: "Internal server Error" });
